f6f6d6c50b92a02c632e1d7f320c522e9d148f8a
azrakrehic <azra.krehic@hotmail.com>
2022-10-11 12:15:51 +0200

    Analysis and design

---
+++ Implementacija/WebApplication1/WebApplication1/Models/Meni.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{ 
+	public class Meni
+	{
+		[NotMapped]
+		public List<Artikl> ponuda { get; set; }
+		public Meni();
+		public ~Meni();
+	}
+}
